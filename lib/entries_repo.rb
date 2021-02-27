1ef67541cf1d43a078bdc2062069fe021fb431af
Greg Hynds <greg.hynds@bbc.co.uk>
2021-02-27 22:16:49 +0000

    Add test for EntriesRepo.all

--- lib/entries_repo.rb
+++ lib/entries_repo.rb
@@ -1,5 +1,8 @@
 class EntriesRepo
     def all
-        []
+        [
+            Entry.new('Link 1', 'https://www.links.com/1'),
+            Entry.new('Link 2', 'https://www.links.com/2'),
+        ]
     end
 end
--- spec/entries_repo_spec.rb
+++ spec/entries_repo_spec.rb
@@ -1,11 +1,16 @@
 require 'entries_repo'
+require 'entry'
 
 RSpec.describe EntriesRepo do
     it "returns an empty list" do
         sut = EntriesRepo.new
+        entries = [
+            Entry.new('Link 1', 'https://www.links.com/1'),
+            Entry.new('Link 2', 'https://www.links.com/2'),
+        ]
 
         result = sut.all
 
-        expect(result).to eq []
+        expect(result).to match_array(entries)
     end
 end
