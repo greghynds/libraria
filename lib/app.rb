c06e7b2f83521ca6b0211aa70ad1ed61966f3500
Greg Hynds <greg.hynds@bbc.co.uk>
2021-02-27 22:38:26 +0000

    Add app class

--- lib/app.rb
+++ lib/app.rb
@@ -1,9 +1,20 @@
+require_relative 'entries_repo'
 require 'sinatra'
 require "sinatra/reloader" if development?
 
 puts "Sinatra: #{Process.pid}"
 
-get '/' do
-  content_type :json
-  { rotation_time: 10 }.to_json
+class Libraria < Sinatra::Base
+
+  before do
+    @repo = EntriesRepo.new
+  end
+
+  get "/" do
+    content_type :json
+    { rotation_time: 10 }.to_json
+  end
+
 end
+
+run Libraria.run!
