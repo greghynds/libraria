3f051426094c7d426411db1f86fa733ae0c91783
Greg Hynds <greg.hynds@bbc.co.uk>
2021-02-28 04:48:44 +0000

    Inline DummySource class in entries_repo_spec

--- spec/entries_repo_spec.rb
+++ spec/entries_repo_spec.rb
@@ -1,6 +1,5 @@
 require 'entries_repo'
 require 'entry'
-require 'dummy_source'
 
 RSpec.describe EntriesRepo do
     it "returns all entries" do
@@ -10,5 +9,17 @@ RSpec.describe EntriesRepo do
         result = sut.all
 
         expect(result).to match_array(source.entries)
+    end
+end
+
+class DummySource
+    attr_reader :entries
+
+    def initialize
+        @entries = [
+            Entry.new('Link 1', 'https://www.links.com/1', 1549054305),
+            Entry.new('Link 2', 'https://www.links.com/2', 1549054310),
+            Entry.new('Link 3', 'https://www.links.com/3', 1549054325),
+        ]
     end
 end
