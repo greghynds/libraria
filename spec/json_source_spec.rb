cd1e9dcfd7043d86c8a5d22c1244de252351b3f0
Greg Hynds <greg.hynds@bbc.co.uk>
2021-02-28 04:47:30 +0000

    Inline dummy data in json_source_spec

--- spec/json_source_spec.rb
+++ spec/json_source_spec.rb
@@ -1,14 +1,14 @@
 require 'entry'
 require 'json_source'
-require 'dummy_source'
 
 RSpec.describe JsonSource do
     it "parses entries from json" do
-        json = '[{"title":"Link 1","link":"https://www.links.com/1","date_created":1549054305},{"title":"Link 2","link":"https://www.links.com/2","date_created":1549054310}]'
+        json = '[{"title":"Link 1","link":"https://www.links.com/1","date_created":1549054305}]'
+        entries = Entry.new( "Link 1", "https://www.links.com/1", 1549054305)
         sut = JsonSource.new(json)
         
         result = sut.entries
 
-        expect(result).to match_array(DummySource.new.entries)
+        expect(result).to match_array([entries])
     end
 end
